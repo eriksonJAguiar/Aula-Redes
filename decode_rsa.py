f32e5365d7d1a364d8406ba68491a00497a6bf1e
eriksonJAguiar <erjulioaguiar@gmail.com>
2019-03-19 10:46:50 -0300

    enviando chaves

--- decode_rsa.py
+++ decode_rsa.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import sys
+import base64
 
 args = sys.argv[1:]
 
@@ -10,10 +11,15 @@ key = RSA.importKey(open('%s'%(str(args[0]))).read())
 cipher = PKCS1_OAEP.new(key)
 
 cipher_msg = open('%s'%(str(args[1])), 'rb').read()
+cipher_msg = base64.b64decode(cipher_msg)
 
-print cipher_msg
 
 msg = cipher.decrypt(str(cipher_msg))
 
 print msg
+
+fd = open("msg.decrypt", "w")
+fd.write(msg)
+fd.close()
+
 
--- encode_rsa.py
+++ encode_rsa.py
@@ -1,18 +1,24 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import sys
+import base64
 
 args = sys.argv[1:]
 
-msg = raw_input('Digite uma mensagem: ')
 
 print "encriptando ..."
 key = RSA.importKey(open('%s'%(str(args[0]))).read())
 cipher = PKCS1_OAEP.new(key)
-ciphertext = cipher.encrypt(msg)
-print ciphertext
 
-fd = open("msg_cript.txt", "wb")
-fd.write(ciphertext)
+fd = open("%s"%(args[1]), "rb")
+msg = fd.read()
 fd.close()
 
+ciphertext = cipher.encrypt(msg)
+ciphertext = base64.b64encode(ciphertext)
+print ciphertext
+
+
+fd = open('msg.crypt','wb')
+fd.write(ciphertext)
+fd.close()
