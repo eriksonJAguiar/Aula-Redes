0a9b319bb60eac16a820f2078b5abba34cf0823d
eriksonJAguiar <erjulioaguiar@gmail.com>
2019-03-21 09:30:45 -0300

    corrigindo bugs cliente

--- sock_client.py
+++ sock_client.py
@@ -1,25 +1,22 @@
 import socket
-HOST = '127.0.0.1'     
+import sys
+
+args = sys.argv[1:]
+
+HOST = '10.62.9.23'     
 PORT = 5000            
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 tcp.send('Hi Server')
-with open('received_file', 'wb') as f:
+with open('%s'%(str(args[0])), 'wb') as f:
     print 'file opened'
     while True:
         print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
+        data = tcp.recv(1024)
         if not data:
             break
         f.write(data)
 
 f.close()
-print('Successfully get the file')
-#print 'Para sair use CTRL+X\n'
-#msg = raw_input()
-#while msg <> '\x18':
-#    tcp.send (msg)
-#    msg = raw_input()
 tcp.close()
